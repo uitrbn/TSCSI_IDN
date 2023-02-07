85363ffa347b9d154ae20e06156054ebb966b3c1
uitrbn <zhaoglong@mail2.sysu.edu.cn>
2023-02-07 20:42:15 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,3 +5,19 @@ def parser_bool(parser, name, default=False):
     group.add_argument('--' + name, dest=name, action='store_true')
     group.add_argument('--no_' + name, dest=name, action='store_false')
     parser.set_defaults(**{name:default})
+
+
+def cifar_config(args):
+    if args.dataset == 'cifar100':
+        if args.idn_noise == 0.6:
+            args.p_threshold = 0.3
+            args.d_threshold = 0.3
+        elif args.idn_noise == 0.4:
+            args.p_threshold = 0.5
+            args.d_threshold = 0
+        elif args.idn_noise == 0.2:
+            args.p_threshold = 0.4
+            args.d_threshold = 0
+    elif args.dataset == 'cifar10':
+        args.p_threshold = 0.4
+        args.d_threshold = 0
